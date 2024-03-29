aeab5f38a8ef63e4835456e913ea021bf4013be2
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-03-29 03:23:05 +0200

    enabled caching for static assets in the whole app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/:all*(svg|jpg|png)",
+                locale: false,
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=9999999999, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
+};
 export default MillionLint.next({
     rsc: true,
 })(nextConfig);
